9101319e11e587beb0c9598c4c61cc8e5adc2cae
almenal <almenal99@gmail.com>
2022-10-26 03:13:07 +0100

    Added basic endpoint for cake, ingredient

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, Column,String,Integer,Float
 import dbase_ops
-from dbase_ops import Employee
+from dbase_ops import Employee, Cake, Ingredient, Assignment
 
 # connect to database
 engine = create_engine('sqlite:///data/db.sqlite3', echo=True)
@@ -11,8 +11,24 @@ dbase_ops.populate_dummy_data(engine)
 
 app = FastAPI()
 
+# region query items by item id -------------------------------------
+
 @app.get("/employees/{employee_id}")
 def fetch_employee_data(employee_id:int):
-    """#TODO Use SQLAlchemy to retrieve employee data"""
+    ""
     with Session(engine) as sess:
         return sess.query(Employee).filter(Employee.id == employee_id).one()
+
+@app.get("/cakes/{cake_id}")
+def fetch_cake_data(cake_id:int):
+    ""
+    with Session(engine) as sess:
+        return sess.query(Cake).filter(Cake.id == cake_id).one()
+
+@app.get("/ingredients/{ingredient_id}")
+def fetch_employee_data(ingredient_id:int):
+    ""
+    with Session(engine) as sess:
+        return sess.query(Ingredient).filter(Ingredient.id == ingredient_id).one()
+
+# endregion -------------------------------------
