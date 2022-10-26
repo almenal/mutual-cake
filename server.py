382d32472c1343eda1ff56d4a2f507e0ea8474b8
almenal <almenal99@gmail.com>
2022-10-26 03:08:38 +0100

    Implement basic employee query by employee id

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
+from click import echo
 from fastapi import FastAPI
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, Column,String,Integer,Float
 import dbase_ops
+from dbase_ops import Employee
 
 # connect to database
-engine = create_engine('sqlite:///data/db.sqlite3')
+engine = create_engine('sqlite:///data/db.sqlite3', echo=True)
 dbase_ops.populate_dummy_data(engine)
 
 app = FastAPI()
@@ -12,4 +14,5 @@ app = FastAPI()
 @app.get("/employees/{employee_id}")
 def fetch_employee_data(employee_id:int):
     """#TODO Use SQLAlchemy to retrieve employee data"""
-    pass
+    with Session(engine) as sess:
+        return sess.query(Employee).filter(Employee.id == employee_id).one()
