25b82a900f3c3599456298cb7dccb0e9d2a3a169
almenal <almenal99@gmail.com>
2022-10-27 01:57:41 +0100

    Avoid HTTP 500 error by using one_or_none

--- server.py
+++ server.py
@@ -16,24 +16,24 @@ app = FastAPI()
 def fetch_employee_data(employee_id:int):
     ""
     with Session(engine) as sess:
-        return sess.query(Employee).filter(Employee.id == employee_id).one()
+        return sess.query(Employee).filter(Employee.id == employee_id).one_or_none()
 
 @app.get("/cakes/{cake_id}")
 def fetch_cake_data(cake_id:int):
     ""
     with Session(engine) as sess:
-        return sess.query(Cake).filter(Cake.id == cake_id).one()
+        return sess.query(Cake).filter(Cake.id == cake_id).one_or_none()
 
 @app.get("/ingredients/{ingredient_id}")
 def fetch_ingredient_data(ingredient_id:int):
     ""
     with Session(engine) as sess:
-        return sess.query(Ingredient).filter(Ingredient.id == ingredient_id).one()
+        return sess.query(Ingredient).filter(Ingredient.id == ingredient_id).one_or_none()
 
 @app.get("/assignments/{assignment_id}")
 def fetch_assignment_data(assignment_id:int):
     ""
     with Session(engine) as sess:
-        return sess.query(Assignment).filter(Assignment.id == assignment_id).one()
+        return sess.query(Assignment).filter(Assignment.id == assignment_id).one_or_none()
 
 # endregion -------------------------------------
